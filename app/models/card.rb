8e8a6d477263da2f029de664c060e2216d7ffe6b
tkasha <ivan.tkachef@gmail.com>
2017-03-29 15:14:53 +0300

    Change validation for card. Callback and method for date in card.

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,5 +1,18 @@
 class Card < ApplicationRecord
-  validates :original_text, :translated_text, presence: true, uniqueness: { case_sensitive: true }
-  validates :original_text, uniqueness: { scope: :translated_text }
-  validates :review_date, presence: true
+  validates :original_text, :translated_text, :review_date, presence: true
+  validate :text_should_be_differ
+
+  before_validation :set_date
+
+  private
+
+  def set_date
+    self.review_date = Date.today + 3.days
+  end
+
+  def text_should_be_differ
+    if original_text.downcase == translated_text.downcase
+      errors.add(:original_text, "Original and translated text should be differ")
+    end
+  end
 end
