3d57a70f3b01fd99239f51fa7e8bd20a0cbcd2d1
tkasha <ivan.tkachef@gmail.com>
2017-03-31 19:12:54 +0300

    Delete validation method from seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,6 @@ def creat_card(original, translated)
   Card.create(original_text: original, translated_text: translated)
 end
 
-def validate_words(original, translated)
-  original == 'German word  ' || translated.size == 1 ? false : true
-end
-
 links = parse_page('http://www.languagedaily.com/learn-german',
                    '//*[@id="jsn-pos-mainmenu"]/div/div/div/ul/li[3]/ul/li/a/attribute::href')
 
@@ -22,7 +18,7 @@ links.each do |link|
                      '//*[@id="jsn-mainbody"]/div[2]/div/div[1]/table/tbody/tr/child::td').to_a
 
   words.in_groups_of(4) do |_, original, translation, _|
-    if validate_words(original.text, translation.text)
+    if original.text != 'German word  '
       creat_card(original.text, translation.text)
     end
   end
