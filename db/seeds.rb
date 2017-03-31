4cfb6d412ccfb475802997a5f54005fa66221358
tkasha <ivan.tkachef@gmail.com>
2017-03-31 16:38:31 +0300

    seeds v2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,35 @@
-page = Nokogiri::HTML(open("http://www.languagedaily.com/learn-german"))
-all_links = page.xpath('//*[@id="jsn-pos-mainmenu"]/div/div/div/ul/li[3]/ul/li/a/attribute::href')
-all_links.each do |link|
-  pages = Nokogiri::HTML(open("http://www.languagedaily.com#{link}"))
-  words = pages.xpath('//*[@id="jsn-mainbody"]/div[2]/div/div[1]/table/tbody/tr/child::td[2]
-                      |//*[@id="jsn-mainbody"]/div[2]/div/div[1]/table/tbody/tr/child::td[3]')
-  a = words.map(&:text)
-  a.each_with_index do |e, i|
-    if i.even? && a[i + 1].size != 1 && e != 'German word  ' && e.downcase != a[i + 1].downcase
-      Card.create!(original_text: e.to_s, translated_text: a[i + 1].to_s)
+def open_the_page(page)
+  Nokogiri::HTML(open(page))
+end
+
+def parsing_page(page, option)
+  open_the_page(page).xpath(option)
+end
+
+def creating_card(original, translated)
+  Card.create!(original_text: original, translated_text: translated)
+end
+
+def validate_words(original, translated)
+  if original.downcase == translated.downcase ||
+  original == 'German word  ' || translated.size == 1
+    false
+  else
+    true
+  end
+end
+
+links = parsing_page('http://www.languagedaily.com/learn-german',
+                     '//*[@id="jsn-pos-mainmenu"]/div/div/div/ul/li[3]/ul/li/a/attribute::href')
+
+links.each do |link|
+  words = parsing_page("http://www.languagedaily.com#{link}",
+                       '//*[@id="jsn-mainbody"]/div[2]/div/div[1]/table/tbody/tr/child::td').to_a
+
+  words.in_groups_of(4) do |_, original, translation, _|
+
+    if validate_words(original.text, translation.text)
+      creating_card(original.text, translation.text)
     end
   end
 end
