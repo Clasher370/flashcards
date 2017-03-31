10e63be3baac15ad11c2b6a77c896ac7348672df
tkasha <ivan.tkachef@gmail.com>
2017-03-31 17:48:58 +0300

    fix houndci

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,15 +15,15 @@ def validate_words(original, translated)
 end
 
 links = parse_page('http://www.languagedaily.com/learn-german',
-                     '//*[@id="jsn-pos-mainmenu"]/div/div/div/ul/li[3]/ul/li/a/attribute::href')
+                   '//*[@id="jsn-pos-mainmenu"]/div/div/div/ul/li[3]/ul/li/a/attribute::href')
 
 links.each do |link|
   words = parse_page("http://www.languagedaily.com#{link}",
-                       '//*[@id="jsn-mainbody"]/div[2]/div/div[1]/table/tbody/tr/child::td').to_a
+                     '//*[@id="jsn-mainbody"]/div[2]/div/div[1]/table/tbody/tr/child::td').to_a
 
   words.in_groups_of(4) do |_, original, translation, _|
     if validate_words(original.text, translation.text)
-    creat_card(original.text, translation.text)
+      creat_card(original.text, translation.text)
     end
   end
 end
