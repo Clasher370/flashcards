7884cb906481a1a32a5b17d0518a904938669a0d
tkasha <ivan.tkachef@gmail.com>
2017-03-31 13:58:46 +0300

    fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,10 +4,10 @@ all_links.each do |link|
   pages = Nokogiri::HTML(open("http://www.languagedaily.com#{link}"))
   words = pages.xpath('//*[@id="jsn-mainbody"]/div[2]/div/div[1]/table/tbody/tr/child::td[2]
                       |//*[@id="jsn-mainbody"]/div[2]/div/div[1]/table/tbody/tr/child::td[3]')
-  a = words.map{ |word|  word.text }
+  a = words.map(&:text)
   a.each_with_index do |e, i|
     if i.even? && a[i + 1].size != 1 && e != 'German word  ' && e.downcase != a[i + 1].downcase
-      Card.create!(original_text: "#{e}", translated_text: "#{a[i + 1]}")
+      Card.create!(original_text: e.to_s, translated_text: a[i + 1].to_s)
     end
   end
 end
