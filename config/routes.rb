6c0bfe3df52c5582e169318f3fc599e0526669f7
tkasha <ivan.tkachef@gmail.com>
2017-04-12 15:38:52 +0300

    Simple auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  resources :user_sessions
+  resources :users
+  resources :cards
+
   root to: 'flashcards#index'
   post 'compare' => 'flashcards#compare'
-  resources :cards
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
+  get 'signup' => 'users#new', :as => :signup
 end
