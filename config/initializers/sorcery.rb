07c502238a0812a4a2814d3048474a9043ff119a
tkasha <ivan.tkachef@gmail.com>
2017-04-27 13:30:10 +0300

    twitter callback in env

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -100,7 +100,7 @@ Rails.application.config.sorcery.configure do |config|
   #
   config.twitter.key = ENV['TWITTER_KEY']
   config.twitter.secret = ENV['TWITTER_SECRET_KEY']
-  config.twitter.callback_url = "http://0.0.0.0:3000/oauth/callback?provider=twitter"
+  config.twitter.callback_url = ENV['TWITTER_CALLBACK']
   config.twitter.user_info_mapping = {:email => "screen_name"}
 
   # config.facebook.key = ENV['FACEBOOK_KEY']
