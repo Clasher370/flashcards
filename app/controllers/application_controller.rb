aba3f6a7199f09dd798bab5864f8f74006e604f4
Clasher370 <tkasha89@gmail.com>
2017-08-25 08:33:24 +0300

    first try locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :require_login
+  before_action :require_login, :set_locale
 
   private
 
   def not_authenticated
     redirect_to login_path, alert: 'Необходимо войти'
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
