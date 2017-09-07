f2889ac902e757022737e0e09e7f6d3163be015b
Clasher370 <tkasha89@gmail.com>
2017-09-07 22:22:25 +0300

    corrected translate

--- app/controllers/flashcards_controller.rb
+++ app/controllers/flashcards_controller.rb
@@ -4,7 +4,7 @@ class FlashcardsController < ApplicationController
     @card = SetCardOnIndex.call(user: current_user).card
   end
 
-  def compareи
+  def compare
     check_card = SuperMemo.new(params[:id], params[:user_text], params[:timer]).algorithm
     respond_to do |format|
       format.html { redirect_to root_path, notice: check_card }
