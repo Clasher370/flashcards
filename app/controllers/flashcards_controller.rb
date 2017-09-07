77909e238c0b5ec62cdbf5d14427eb912db72370
Clasher370 <tkasha89@gmail.com>
2017-09-07 22:23:37 +0300

    ajax request for action compare

--- app/controllers/flashcards_controller.rb
+++ app/controllers/flashcards_controller.rb
@@ -1,14 +1,18 @@
 # app/controllers/flashcards_controller.rb
 class FlashcardsController < ApplicationController
-  def index
-    @card = SetCardOnIndex.call(user: current_user).card
-  end
+  before_action :set_card, only: [:index, :compare]
 
   def compare
-    check_card = SuperMemo.new(params[:id], params[:user_text], params[:timer]).algorithm
+    flash.now[:notice] = SuperMemo.new(params[:id], params[:user_text], params[:timer]).algorithm
     respond_to do |format|
-      format.html { redirect_to root_path, notice: check_card }
+      format.html { redirect_to root_path }
       format.js
     end
+  end
+
+  private
+
+  def set_card
+    @card = SetCardOnIndex.call(user: current_user).card
   end
 end
